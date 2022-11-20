b1470a0ba8710943cb8d2f5a97faaf413baaf7e4
MartinNgu <117178838+MartinNgu@users.noreply.github.com>
2022-11-20 12:35:08 +0100

    Add files via upload

---
+++ Services/ReservationsService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\Reservation;
+use DateTime;
+
+class ReservationsService
+{
+    /**
+     * Create or update a reservation.
+     */
+    public function setReservation(?string $id, string $author, string $client, string $rescitystart, string $rescityend, string $dateres): bool
+    {
+        $isOk = false;
+
+        $dataBaseService = new DataBaseService();
+        $dateresDateTime = new DateTime($dateres);
+        if (empty($id)) {
+            $isOk = $dataBaseService->createUser($author, $client, $rescitystart, $rescityend, $dateres);
+        } else {
+            $isOk = $dataBaseService->updateUser($id, $author, $client, $rescitystart, $rescityend, $dateres);
+        }
+
+        return $isOk;
+    }
+
+    /**
+     * Return all reservation.
+     */
+    public function getReservations(): array
+    {
+        $reservations = [];
+
+        $dataBaseService = new DataBaseService();
+        $reservationsDTO = $dataBaseService->getReservations();
+        if (!empty($reservationsDTO)) {
+            foreach ($reservationsDTO as $reservationsDTO) {
+                $reservation = new Reservation();
+                $reservation->setId($reservationsDTO['id']);
+                $reservation->setAuthor($reservationsDTO['author']);
+                $reservation->setClient($reservationsDTO['client']);
+                $reservation->setRescitystart($reservationsDTO['rescitystart']);
+                $reservation->setRescityend($reservationsDTO['rescityend']);
+                $dateres = new DateTime($reservationsDTO['dateres']);
+                if ($dateres !== false) {
+                    $reservation->setbirthday($dateres);
+                }
+                $reservations[] = $reservation;
+            }
+        }
+
+        return $reservations;
+    }
+
+    /**
+     * Delete a reservation.
+     */
+    public function deleteReservation(string $id): bool
+    {
+        $isOk = false;
+
+        $dataBaseService = new DataBaseService();
+        $isOk = $dataBaseService->deleteReservation($id);
+
+        return $isOk;
+    }
+}
